d0bf0abc1393269815035c4c8663b10af43b21ca
CarrieWei <708872851@qq.com>
2016-05-29 18:53:08 +0800

    Action

---
+++ Action.h
@@ -0,0 +1,23 @@
+//
+// Created by д╚аж on 16/5/28.
+//
+
+#ifndef STUDENT_PERSONAL_FINANCIAL_MANAGEMENT_ACTION_H
+#define STUDENT_PERSONAL_FINANCIAL_MANAGEMENT_ACTION_H
+#include "User.h"
+#include "Localtime.h"
+#include <fstream>
+
+class Action {
+public:
+	User demo;
+	void userLogin();
+	void startApp();
+	void createUser();
+	void createUser(std::string);
+	bool initializeFile();
+	void saveSet();
+private:
+	std::ifstream loadUser;
+};
+#endif //STUDENT_PERSONAL_FINANCIAL_MANAGEMENT_ACTION_H
